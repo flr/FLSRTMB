0325b66e43a3e9a6111daf70beefc255bbfe14da
Henning-Winker <henning.winker@gmail.com>
2021-10-30 16:47:31 +0200

    added function sprFy with byage option

--- R/utils.R
+++ R/utils.R
@@ -22,10 +22,11 @@ to_logits <- function(h){
 #'
 #' Function to compute annual spr0 
 #' @param object class FLStock
+#' @param byage if TRUE it return spr0_at_age
 #' @return FLQuant with annual spr0y  
 #' @export
 #' @author Laurence Kell
-spr0y<-function(object){
+spr0y<-function(object,byage=FALSE){
   survivors=exp(-apply(m(object),2,cumsum))
   survivors[-1]=survivors[-dim(survivors)[1]]
   survivors[1]=1
@@ -35,5 +36,29 @@ spr0y<-function(object){
   
   fec=mat(object)*stock.wt(object)*exp(-m(object)*m.spwn(object))
   
-  rtn=apply(fec*survivors,2,sum)
+  if(byage) rtn = fec * survivors
+  if(!byage) rtn =  apply(fec * survivors, 2, sum)
+  rtn}
+
+
+
+#' spr0y()
+#'
+#' Function to compute annual sprF as function of F_a
+#' @param object class FLStock
+#' @param byage if TRUE it return sprF_at_age
+#' @return FLQuant with annual sprFy  
+#' @export
+#' @author Henning Winker and Laurence Kell 
+sprFy = function(object,byage = FALSE){
+  survivors = exp(-apply(m(object)+harvest(object), 2, cumsum))
+  survivors[-1] = survivors[-dim(survivors)[1]]
+  survivors[1] = 1
+  expZ = exp(-(m(object[dim(m(object))[1]])+harvest(object[dim(m(object))[1]])))
+  if (!is.na(range(object)["plusgroup"])) 
+    survivors[dim(m(object))[1]] = survivors[dim(m(object))[1]] * 
+    (-1/(expZ - 1))
+  fec = mat(object) * stock.wt(object) * exp(-(m(object)+harvest(object)) * m.spwn(object))
+  if(byage) rtn = fec * survivors
+  if(!byage) rtn =  apply(fec * survivors, 2, sum)
   rtn}
